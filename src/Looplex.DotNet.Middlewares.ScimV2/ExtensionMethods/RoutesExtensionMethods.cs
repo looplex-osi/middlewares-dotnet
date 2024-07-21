f94437348348bdb57764a5275d524154a2a08763
Guilherme Câmara <guilherme.camara@looplex.com.br>
2024-07-20 21:15:32 -0300

    fix: upgrade core packages

--- src/Looplex.DotNet.Middlewares.ScimV2/ExtensionMethods/RoutesExtensionMethods.cs
+++ src/Looplex.DotNet.Middlewares.ScimV2/ExtensionMethods/RoutesExtensionMethods.cs
@@ -7,6 +7,7 @@ using Looplex.DotNet.Core.Application.Abstractions.Services;
 using Looplex.DotNet.Core.Middlewares;
 using Looplex.DotNet.Core.WebAPI.Middlewares;
 using Looplex.DotNet.Core.WebAPI.Routes;
+using Looplex.DotNet.Core.Common.Utils;
 
 namespace Looplex.DotNet.Middlewares.ScimV2.ExtensionMethods;
 
@@ -20,7 +21,7 @@ public static class RoutesExtensionMethods
 
         await service.GetAllAsync(context, cancellationToken);
 
-        await httpContext.Response.WriteAsJsonAsync(context.Result, cancellationToken);
+        await httpContext.Response.WriteAsJsonAsync((string)context.Result!, HttpStatusCode.OK);
     };
 
     private static MiddlewareDelegate GetByIdMiddleware<TService>()
@@ -34,7 +35,7 @@ public static class RoutesExtensionMethods
 
         await service.GetByIdAsync(context, cancellationToken);
 
-        await httpContext.Response.WriteAsJsonAsync(context.Result, cancellationToken);
+        await httpContext.Response.WriteAsJsonAsync((string)context.Result!, HttpStatusCode.OK);
     };
 
     private static MiddlewareDelegate PostMiddleware<TService>(
@@ -74,50 +75,49 @@ public static class RoutesExtensionMethods
         ScimV2RouteOptions options)
         where TService : ICrudService
     {
-
         List<MiddlewareDelegate> getMiddlewares = [
             AuthenticationMiddlewares.AuthenticateMiddleware, CoreMiddlewares.PaginationMiddleware];
         getMiddlewares.AddRange(options.OptionsForGet?.Middlewares ?? []);
         getMiddlewares.Add(GetMiddleware<TService>());
         app.MapGet(
-                resource,
-                new RouteBuilderOptions
-                {
-                    Services = options.OptionsForGet?.Services ?? [],
-                    Middlewares = getMiddlewares.ToArray()
-                });
+            resource,
+            new RouteBuilderOptions
+            {
+                Services = options.OptionsForGet?.Services ?? [],
+                Middlewares = getMiddlewares.ToArray()
+            });
 
         List<MiddlewareDelegate> getByIdMiddlewares = [AuthenticationMiddlewares.AuthenticateMiddleware];
         getByIdMiddlewares.AddRange(options.OptionsForGetById?.Middlewares ?? []);
         getByIdMiddlewares.Add(GetByIdMiddleware<TService>());
         app.MapGet(
-                $"{resource}/{{id}}",
-                new RouteBuilderOptions
-                {
-                    Services = options.OptionsForGetById?.Services ?? [],
-                    Middlewares = getByIdMiddlewares.ToArray()
-                });
+            $"{resource}/{{id}}",
+            new RouteBuilderOptions
+            {
+                Services = options.OptionsForGetById?.Services ?? [],
+                Middlewares = getByIdMiddlewares.ToArray()
+            });
 
         List<MiddlewareDelegate> postMiddlewares = [AuthenticationMiddlewares.AuthenticateMiddleware];
         postMiddlewares.AddRange(options.OptionsForPost?.Middlewares ?? []);
         postMiddlewares.Add(PostMiddleware<TService>(resource));
         app.MapPost(
-                resource,
-                new RouteBuilderOptions
-                {
-                    Services = options.OptionsForPost?.Services ?? [],
-                    Middlewares = postMiddlewares.ToArray()
-                });
+            resource,
+            new RouteBuilderOptions
+            {
+                Services = options.OptionsForPost?.Services ?? [],
+                Middlewares = postMiddlewares.ToArray()
+            });
 
         List<MiddlewareDelegate> deleteMiddlewares = [AuthenticationMiddlewares.AuthenticateMiddleware];
         deleteMiddlewares.AddRange(options.OptionsForDelete?.Middlewares ?? []);
         deleteMiddlewares.Add(DeleteMiddleware<TService>());
         app.MapDelete(
-                $"{resource}/{{id}}",
-                new RouteBuilderOptions
-                {
-                    Services = options.OptionsForDelete?.Services ?? [],
-                    Middlewares = deleteMiddlewares.ToArray()
-                });
+            $"{resource}/{{id}}",
+            new RouteBuilderOptions
+            {
+                Services = options.OptionsForDelete?.Services ?? [],
+                Middlewares = deleteMiddlewares.ToArray()
+            });
     }
 }
