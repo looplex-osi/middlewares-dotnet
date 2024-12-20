cbdfc7525aa7342514f02a01174e8aa2c5ce28a6
Rafael Imakawa <rafael.imakawa@looplex.com.br>
2024-12-20 20:50:17 -0300

    test: AB#28655 tests for AuthorizationMiddleware and DomainProviderService

--- test/Looplex.DotNet.Middlewares.OAuth2.UnitTests/Middlewares/AuthorizationMiddlewareTests.cs
+++ test/Looplex.DotNet.Middlewares.OAuth2.UnitTests/Middlewares/AuthorizationMiddlewareTests.cs
@@ -15,6 +15,8 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Casbin;
+using System.Reflection;
+using FluentAssertions;
 
 namespace Looplex.DotNet.Middlewares.OAuth2.UnitTests.Middlewares
 {
@@ -56,7 +58,8 @@ namespace Looplex.DotNet.Middlewares.OAuth2.UnitTests.Middlewares
 
             dynamic state = new ExpandoObject();
             state.HttpContext = _httpContext;
-            
+            _httpContext.Request.Headers.Authorization = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IlJhZmFlbCBSYWZhZWwiLCJlbWFpbCI6InJhZmFlbC5pbWFrYXdhQGxvb3BsZXguY29tLmJyIiwiUGhvdG8iOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20vdjEuMC9tZS9waG90by8kdmFsdWUiLCJuYmYiOjE3MzM5NDIyMjAsImV4cCI6MTczMzk0NTgyMCwiaWF0IjoxNzMzOTQyMjIwLCJpc3MiOiJkb3RuZXQubG9vcGxleC5jb20uYnIiLCJhdWQiOiJzYWFzLmxvb3BsZXguY29tLmJyIn0.NuDSJbMUVhqdZGK8vb-rAn0ETv-Vi3raakQdB-yGZBLdEQ3luzdw8zytD6VGhM2-JI3VLZ_I0ZkUBUurOXGzbwXy6fMAMgLZOz4WDJ6Boa3LlbJkSqyQumu9ACHu3bEZMSwvLuqWgL1_AT_9NcJsxPpj63KNqmhZwx30hYsTm1SsYNU49XeeBPDzGeaWk6N44qnrcNjPapLhbGLBi6iaGJOyniGCMjCZFd8Pwv3JF3peZ_xW7omw6XA5CTpzbuNg50Eo3D0ZCwwJoC4Kid-LvZ0TWjARrCChm3Wdv81i6bXDxKgjIcqtkCvz4G1wzQKb9hE3HiaQEx_3tPPbxj68_nQqMh_ZP-pDbrHJRzkZy5jBKw1SutrSBuA3_HDATYPzF9feE-DvrrZF42aZIFTYZ7ndN1qJxfdOb4mSk3J1r9qTuCS7h5t6UF0qTOkShxU01Ve3F4YmfVsop0qsaGJicEh0vf142pWmLFjy4IxNHb-Nm_rk6oImckLO0qyI6-lVMI-C1x5ImsvMX6gT0nJqOF872ewbSykmS2PGkatOBHP7DwI_T612kKMCAoVoB7D46Me1AxvDdNi_4pGnTdoEabNAF9OdrG6CXGQtCilwRcNV8bWGdprcMhgfXpq5EnStRYva4SVPida8fEQDzfFL-I560G1tV1owYKgxY4dXO2k";
+
             _context.State.Returns(state);
 
             // Set up the next middleware delegate
@@ -64,22 +67,159 @@ namespace Looplex.DotNet.Middlewares.OAuth2.UnitTests.Middlewares
         }
 
         [TestMethod]
-        public async Task AuthenticateMiddleware_ValidToken_CallsNextMiddleware()
+        public async Task AuthenticateMiddleware_GetResourceFromURL_LastPathAsID()
         {
-            // Arrange
-            string validToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IlJhZmFlbCBSYWZhZWwiLCJlbWFpbCI6InJhZmFlbC5pbWFrYXdhQGxvb3BsZXguY29tLmJyIiwiUGhvdG8iOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20vdjEuMC9tZS9waG90by8kdmFsdWUiLCJuYmYiOjE3MzM5NDIyMjAsImV4cCI6MTczMzk0NTgyMCwiaWF0IjoxNzMzOTQyMjIwLCJpc3MiOiJkb3RuZXQubG9vcGxleC5jb20uYnIiLCJhdWQiOiJzYWFzLmxvb3BsZXguY29tLmJyIn0.NuDSJbMUVhqdZGK8vb-rAn0ETv-Vi3raakQdB-yGZBLdEQ3luzdw8zytD6VGhM2-JI3VLZ_I0ZkUBUurOXGzbwXy6fMAMgLZOz4WDJ6Boa3LlbJkSqyQumu9ACHu3bEZMSwvLuqWgL1_AT_9NcJsxPpj63KNqmhZwx30hYsTm1SsYNU49XeeBPDzGeaWk6N44qnrcNjPapLhbGLBi6iaGJOyniGCMjCZFd8Pwv3JF3peZ_xW7omw6XA5CTpzbuNg50Eo3D0ZCwwJoC4Kid-LvZ0TWjARrCChm3Wdv81i6bXDxKgjIcqtkCvz4G1wzQKb9hE3HiaQEx_3tPPbxj68_nQqMh_ZP-pDbrHJRzkZy5jBKw1SutrSBuA3_HDATYPzF9feE-DvrrZF42aZIFTYZ7ndN1qJxfdOb4mSk3J1r9qTuCS7h5t6UF0qTOkShxU01Ve3F4YmfVsop0qsaGJicEh0vf142pWmLFjy4IxNHb-Nm_rk6oImckLO0qyI6-lVMI-C1x5ImsvMX6gT0nJqOF872ewbSykmS2PGkatOBHP7DwI_T612kKMCAoVoB7D46Me1AxvDdNi_4pGnTdoEabNAF9OdrG6CXGQtCilwRcNV8bWGdprcMhgfXpq5EnStRYva4SVPida8fEQDzfFL-I560G1tV1owYKgxY4dXO2k";
-            _httpContext.Request.Headers.Authorization = $"Bearer {validToken}";
-            _httpContext.Request.Headers.Add("X-looplex-tenant", "TestDomain");
-            _httpContext.Request.Path = "/cases/24/andamentos/32";
-            _httpContext.Request.Method = "POST";
+            
+            MethodInfo getResourceFromURL = typeof(AuthorizationMiddleware).GetMethod("GetResourceFromURL", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance);
 
-            // Act
-            await AuthorizationMiddleware.AuthorizeMiddleware(_context, CancellationToken.None, _next);
+            getResourceFromURL.Should().NotBeNull();
+            HttpContext httpContext = _context.State.HttpContext;
+            httpContext.Request.Path = "/cases/24/andamentos/32";
 
-            // Assert
-            await _next.Received(1).Invoke();
+            string result = (string)getResourceFromURL.Invoke(null, new object[] {_context });
+
+            result.Should().Be("andamentos/32");
         }
 
-        
+        [TestMethod]
+        public async Task AuthenticateMiddleware_GetResourceFromURL_LastPathAsResource()
+        {
+
+            MethodInfo getResourceFromURL = typeof(AuthorizationMiddleware).GetMethod("GetResourceFromURL", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            getResourceFromURL.Should().NotBeNull();
+            HttpContext httpContext = _context.State.HttpContext;
+            httpContext.Request.Path = "/cases/24/andamentos";
+
+            string result = (string)getResourceFromURL.Invoke(null, new object[] { _context });
+
+            result.Should().Be("andamentos");
+        }
+
+
+
+        [TestMethod]
+        public async Task AuthenticateMiddleware_GetResourceFromURL_SingleElementPath()
+        {
+
+            MethodInfo getResourceFromURL = typeof(AuthorizationMiddleware).GetMethod("GetResourceFromURL", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            getResourceFromURL.Should().NotBeNull();
+            HttpContext httpContext = _context.State.HttpContext;
+            httpContext.Request.Path = "/cases";
+
+            string result = (string)getResourceFromURL.Invoke(null, new object[] { _context });
+
+            result.Should().Be("cases");
+        }
+
+        [TestMethod]
+        public async Task AuthenticateMiddleware_GetResourceFromURL_DoubleElementPath()
+        {
+
+            MethodInfo getResourceFromURL = typeof(AuthorizationMiddleware).GetMethod("GetResourceFromURL", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            getResourceFromURL.Should().NotBeNull();
+            HttpContext httpContext = _context.State.HttpContext;
+            httpContext.Request.Path = "/cases/2";
+
+            string result = (string)getResourceFromURL.Invoke(null, new object[] { _context });
+
+            result.Should().Be("cases/2");
+        }
+
+
+        [TestMethod]
+        public async Task AuthenticateMiddleware_GetUserIdFromToken_SuccessExample()
+        {
+
+            MethodInfo getResourceFromURL = typeof(AuthorizationMiddleware).GetMethod("GetUserIdFromToken", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            getResourceFromURL.Should().NotBeNull();
+            HttpContext httpContext = _context.State.HttpContext;
+            
+
+            string result = (string)getResourceFromURL.Invoke(null, new object[] { _context });
+
+            result.Should().Be("rafael.imakawa@looplex.com.br");
+        }
+
+        [TestMethod]
+        public async Task AuthenticateMiddleware_ConvertActionToMethod_GET()
+        {
+
+            MethodInfo getResourceFromURL = typeof(AuthorizationMiddleware).GetMethod("ConvertActionToMethod", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            getResourceFromURL.Should().NotBeNull();
+            HttpContext httpContext = _context.State.HttpContext;
+            httpContext.Request.Method = "GET";
+
+            string result = (string)getResourceFromURL.Invoke(null, new object[] { _context });
+
+            result.Should().Be("read");
+        }
+
+        [TestMethod]
+        public async Task AuthenticateMiddleware_ConvertActionToMethod_POST()
+        {
+
+            MethodInfo getResourceFromURL = typeof(AuthorizationMiddleware).GetMethod("ConvertActionToMethod", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            getResourceFromURL.Should().NotBeNull();
+            HttpContext httpContext = _context.State.HttpContext;
+            httpContext.Request.Method = "POST";
+
+            string result = (string)getResourceFromURL.Invoke(null, new object[] { _context });
+
+            result.Should().Be("write");
+        }
+
+        [TestMethod]
+        public async Task AuthenticateMiddleware_ConvertActionToMethod_PUT()
+        {
+
+            MethodInfo getResourceFromURL = typeof(AuthorizationMiddleware).GetMethod("ConvertActionToMethod", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            getResourceFromURL.Should().NotBeNull();
+            HttpContext httpContext = _context.State.HttpContext;
+            httpContext.Request.Method = "PUT";
+
+            string result = (string)getResourceFromURL.Invoke(null, new object[] { _context });
+
+            result.Should().Be("write");
+        }
+        [TestMethod]
+        public async Task AuthenticateMiddleware_ConvertActionToMethod_DELETE()
+        {
+
+            MethodInfo getResourceFromURL = typeof(AuthorizationMiddleware).GetMethod("ConvertActionToMethod", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            getResourceFromURL.Should().NotBeNull();
+            HttpContext httpContext = _context.State.HttpContext;
+            httpContext.Request.Method = "DELETE";
+
+            string result = (string)getResourceFromURL.Invoke(null, new object[] { _context });
+
+            result.Should().Be("delete");
+        }
+
+        [TestMethod]
+        public async Task AuthenticateMiddleware_ConvertActionToMethod_Should_Throw_Exception_When_No_Method_Is_Provided()
+        {
+
+            MethodInfo getResourceFromURL = typeof(AuthorizationMiddleware).GetMethod("ConvertActionToMethod", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            getResourceFromURL.Should().NotBeNull();
+            HttpContext httpContext = _context.State.HttpContext;
+            httpContext.Request.Method = null;
+
+            var ex =  Assert.ThrowsException<TargetInvocationException>( () =>
+            
+            (string)getResourceFromURL.Invoke(null, new object[] { _context }));
+
+            Assert.AreEqual("HttpMethod not found", ex.InnerException.Message);
+        }
+
+
     }
 }
