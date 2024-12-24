d1fb0f2cf416d4af4e899916349ce975fefa282e
Rafael Imakawa <rafael.imakawa@looplex.com.br>
2024-12-23 22:08:24 -0300

    refactor: mock bearer and other ranbit refactoring

--- test/Looplex.DotNet.Middlewares.OAuth2.UnitTests/Middlewares/AuthorizationMiddlewareTests.cs
+++ test/Looplex.DotNet.Middlewares.OAuth2.UnitTests/Middlewares/AuthorizationMiddlewareTests.cs
@@ -58,7 +58,7 @@ namespace Looplex.DotNet.Middlewares.OAuth2.UnitTests.Middlewares
 
             dynamic state = new ExpandoObject();
             state.HttpContext = _httpContext;
-            _httpContext.Request.Headers.Authorization = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IlJhZmFlbCBSYWZhZWwiLCJlbWFpbCI6InJhZmFlbC5pbWFrYXdhQGxvb3BsZXguY29tLmJyIiwiUGhvdG8iOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20vdjEuMC9tZS9waG90by8kdmFsdWUiLCJuYmYiOjE3MzM5NDIyMjAsImV4cCI6MTczMzk0NTgyMCwiaWF0IjoxNzMzOTQyMjIwLCJpc3MiOiJkb3RuZXQubG9vcGxleC5jb20uYnIiLCJhdWQiOiJzYWFzLmxvb3BsZXguY29tLmJyIn0.NuDSJbMUVhqdZGK8vb-rAn0ETv-Vi3raakQdB-yGZBLdEQ3luzdw8zytD6VGhM2-JI3VLZ_I0ZkUBUurOXGzbwXy6fMAMgLZOz4WDJ6Boa3LlbJkSqyQumu9ACHu3bEZMSwvLuqWgL1_AT_9NcJsxPpj63KNqmhZwx30hYsTm1SsYNU49XeeBPDzGeaWk6N44qnrcNjPapLhbGLBi6iaGJOyniGCMjCZFd8Pwv3JF3peZ_xW7omw6XA5CTpzbuNg50Eo3D0ZCwwJoC4Kid-LvZ0TWjARrCChm3Wdv81i6bXDxKgjIcqtkCvz4G1wzQKb9hE3HiaQEx_3tPPbxj68_nQqMh_ZP-pDbrHJRzkZy5jBKw1SutrSBuA3_HDATYPzF9feE-DvrrZF42aZIFTYZ7ndN1qJxfdOb4mSk3J1r9qTuCS7h5t6UF0qTOkShxU01Ve3F4YmfVsop0qsaGJicEh0vf142pWmLFjy4IxNHb-Nm_rk6oImckLO0qyI6-lVMI-C1x5ImsvMX6gT0nJqOF872ewbSykmS2PGkatOBHP7DwI_T612kKMCAoVoB7D46Me1AxvDdNi_4pGnTdoEabNAF9OdrG6CXGQtCilwRcNV8bWGdprcMhgfXpq5EnStRYva4SVPida8fEQDzfFL-I560G1tV1owYKgxY4dXO2k";
+            _httpContext.Request.Headers.Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhZmFlbC5pbWFrYXdhQGxvb3BsZXguY29tLmJyIn0.mock-signature";
 
             _context.State.Returns(state);
 
@@ -110,13 +110,11 @@ namespace Looplex.DotNet.Middlewares.OAuth2.UnitTests.Middlewares
         public void AuthenticateMiddleware_GetResourceFromURL_SingleElementPath()
         {
 
-            MethodInfo getResourceFromURL = typeof(AuthorizationMiddleware).GetMethod("GetResourceFromURL", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance)!;
-
-            getResourceFromURL.Should().NotBeNull();
+            var getResourceFromURL = GetPrivateMethod("GetResourceFromURL");
             HttpContext httpContext = _context.State.HttpContext;
             httpContext.Request.Path = "/cases";
 
-            string? result = (string?)getResourceFromURL.Invoke(null, new object[] { _context });
+            string result = InvokeMethodWithContext(getResourceFromURL);
 
             result.Should().Be("cases");
         }
